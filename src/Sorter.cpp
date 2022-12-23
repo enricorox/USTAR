7369aa4cab000cc2935af8540ada675cb5ec017c
Enrico R <enricorrx@gmail.com>
2022-12-23 11:10:29 +0100

    added Sorter.cpp for sorting seeds and successors

---
+++ src/Sorter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by enrico on 22/12/22.
+//
+
+#include "Sorter.h"
---
+++ src/Sorter.h
@@ -0,0 +1,8 @@
+//
+// Created by enrico on 22/12/22.
+//
+
+#ifndef USTAR_SORTER_H
+#define USTAR_SORTER_H
+
+#endif //USTAR_SORTER_H
