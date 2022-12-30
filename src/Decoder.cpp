fcc9a2f9398957554444e31d1cd1ec094a9d3bd5
Enrico R <enricorrx@gmail.com>
2022-12-30 15:42:13 +0100

    comments

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -30,7 +30,9 @@ void Decoder::extract_kmers_and_counts(const string &output_file_name) {
         exit(EXIT_FAILURE);
     }
 
+    // line read in fasta file
     string simplitig;
+    // number of kmers incremented at each written line
     size_t n_kmers = 0;
     while(true){
         // break if end of file
@@ -41,10 +43,12 @@ void Decoder::extract_kmers_and_counts(const string &output_file_name) {
         if(simplitig[0] == '>')
             continue;
 
+        // extract kmers in a simplitig
         for(size_t i = 0; i < simplitig.size() - kmer_size + 1; i++){
             string kmer = simplitig.substr(i, kmer_size);
             string kmer_rc = DBG::reverse_complement(kmer);
-            // output canonical kmer
+
+            // output canonical kmer only
             // NOTE that DSK computes canonical kmers with A<C<T<G order! Then I use jellyfish for validation
             output << ((kmer < kmer_rc) ? kmer : kmer_rc);
 
@@ -61,6 +65,7 @@ void Decoder::extract_kmers_and_counts(const string &output_file_name) {
         cerr << "extract_kmers_and_counts(): There are too much counts" << endl;
         exit(EXIT_FAILURE);
     }
+
     simplitigs.close();
     output.close();
 }
@@ -75,6 +80,7 @@ void Decoder::decode(encoding_t encoding) {
 
     switch(encoding){
         case encoding_t::PLAIN: {
+                // simply read integers
                 uint32_t c;
                 while (counts_file >> c){
                     counts.push_back(c);
@@ -84,19 +90,27 @@ void Decoder::decode(encoding_t encoding) {
         case encoding_t::RLE:{
                 string line;
                 while(getline(counts_file, line)){
+                    // is this line a "S:R"?
                     size_t pos = line.find(RLE_SEPARATOR);
-                    if(pos == string::npos){ // no ":" in the line
+                    if(pos == string::npos){ // no run
                         int symbol = atoi(line.c_str());
                         counts.push_back(symbol);
+
                         if(debug && symbol < 2){
                             cout << "Warning: found count = " << symbol << "\n";
                         }
-                    } else{
+                    } else{ // there is a run here
+                        // break line in two string where there is the separator
                         line[pos] = '\0';
+
+                        // read symbol and run length
                         int symbol = atoi(line.c_str());
                         int run = atoi(line.c_str() + pos + 1);
+
                         if(debug && run < 2)
                             cout << "Warning: found " << run << "-length run \n";
+
+                        // save multiple symbols
                         for(int i = 0; i < run; i++)
                             counts.push_back(symbol);
                     }
--- src/Decoder.h
+++ src/Decoder.h
@@ -20,10 +20,25 @@ class Decoder{
     vector<uint32_t> counts;
 
 public:
+    /**
+     * Decode and extract kmers from simplitigs
+     * @param fasta_file_name USTAR or UST fasta file
+     * @param counts_file_name USTAR or UST counts file
+     * @param kmer_size kmer size
+     * @param debug debug flag
+     */
     Decoder(const string &fasta_file_name, const string &counts_file_name, int kmer_size, bool debug);
 
+    /**
+     * Extract kmers and counts and write them to a txt file
+     * @param output_file_name where to put kmers and counts
+     */
     void extract_kmers_and_counts(const string &output_file_name);
 
+    /**
+     * Reads and decode counts
+     * @param encoding encoding used by USTAR
+     */
     void decode(encoding_t encoding);
 };
 #endif //USTAR_DECODER_H
