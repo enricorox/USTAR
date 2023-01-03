379b05f0b6b42ade298940659c14ff28157b3df7
Enrico R <enricorrx@gmail.com>
2023-01-03 15:08:56 +0100

    added Postprocessor

---
+++ src/Postprocessor.cpp
@@ -0,0 +1,10 @@
+//
+// Created by enrico on 31/12/22.
+//
+
+#include "Postprocessor.h"
+
+
+Postprocessor::Postprocessor(const vector<string> &simplitigs, const vector<vector<uint32_t>> &counts) {
+
+}
