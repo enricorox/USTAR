e36a1e131cfc0270fa31f3cd8b5bb2ed84dda1d5
Enrico R <enricorrx@gmail.com>
2023-01-11 16:31:46 +0100

    added -c +c in help

--- src/ustar.cpp
+++ src/ustar.cpp
@@ -56,6 +56,8 @@ void print_help(const params_t &params){
     cout << "       =a              choose the seed with most similar abundance to the last used node\n";
     cout << "       -l              choose the seed with smaller length\n";
     cout << "       +l              choose the seed with bigger length\n";
+    cout << "       -c              choose the seed with less arcs\n";
+    cout << "       +c              choose the seed with more arcs\n";
     cout << "\n";
 
     cout << "   -x  extending method [" << inv_map<extending_method_t>(extending_method_names, params.extending_method) << "]\n";
@@ -64,6 +66,8 @@ void print_help(const params_t &params){
     cout << "       =a              choose the successor with most similar abundance to the last used node\n";
     cout << "       -l              choose the successor with smaller length\n";
     cout << "       +l              choose the successor with bigger length\n";
+    cout << "       -c              choose the successor with less arcs\n";
+    cout << "       +c              choose the successor with more arcs\n";
     cout << "\n";
 
     cout << "   -I  number of iterations [" << params.iterations << "]\n";
